8776f528dc97f998344a981959349e726f1ede26
vargha1 <vargha.rahavi@gmail.com>
2024-11-16 12:51:32 +0330

    VR

--- main.js
+++ main.js
@@ -724,60 +724,54 @@ window.openDetails = index => {
   controls.enableRotate = true
   controls.enablePan = true
   controls.enableZoom = true
-  renderer.setSize(window.innerWidth, window.innerHeight - 52 - 150)
+  // renderer.setSize(window.innerWidth, window.innerHeight - 52 - 150)
   if (index == 1) {
     name.innerHTML = "Chocolate"
     desc.innerHTML = "description"
     price.innerHTML = "100$"
-    ar.setAttribute("src", "./chocolate/chocolate_milka.glb")
+    ar.setAttribute("src", "chocolate/chocolate_milka.glb")
   }
   if (index == 2) {
     name.innerHTML = "Black Chocolate"
     desc.innerHTML = "description"
     price.innerHTML = "100$"
-    ar.setAttribute("src", "./blackChocolate/scene.gltf")
-    scene.getObjectByName("Sketchfab_Scene2").scale.set(30, 30, 30)
-    scene.getObjectByName("Sketchfab_Scene2").position.set(-5, 15, 35)
+    ar.setAttribute("src", "blackChocolate/scene.gltf")
   }
   if (index == 3) {
     name.innerHTML = "Nestle"
     desc.innerHTML = "description"
     price.innerHTML = "100$"
-    ar.setAttribute("src", "./nestle/scene.gltf")
-    scene.getObjectByName("Sketchfab_Scene3").scale.set(200, 200, 200)
+    ar.setAttribute("src", "nestle/scene.gltf")
   }
   if (index == 4) {
     name.innerHTML = "Dark Chocolate"
     desc.innerHTML = "description"
     price.innerHTML = "100$"
-    ar.setAttribute("src", "./darkChocolate/scene.gltf")
-    // scene.getObjectByName("Sketchfab_Scene4").scale.set(0.75, 1, 1)
+    ar.setAttribute("src", "darkChocolate/scene.gltf")
   }
   if (index == 5) {
     name.innerHTML = "Juice"
     desc.innerHTML = "description"
     price.innerHTML = "100$"
-    ar.setAttribute("src", "./juice/floor_Gltf.gltf")
-    // scene.getObjectByName("Sketchfab_Scene5").scale.set(0.75, 1, 1)
+    ar.setAttribute("src", "juice/floor_Gltf.gltf")
   }
   if (index == 6) {
     name.innerHTML = "Mobile"
     desc.innerHTML = "description"
     price.innerHTML = "100$"
-    ar.setAttribute("src", "./mobile/scene.gltf")
-    scene.getObjectByName("Sketchfab_Scene6").scale.set(350, 450, 350)
+    ar.setAttribute("src", "mobile/scene.gltf")
   }
   if (index == 7) {
     name.innerHTML = "Suzuki"
     desc.innerHTML = "description"
     price.innerHTML = "100$"
-    ar.setAttribute("src", "./suzuki/scene.gltf")
+    ar.setAttribute("src", "suzuki/scene.gltf")
   }
   if (index == 8) {
     name.innerHTML = "Cat Food"
     desc.innerHTML = "description"
     price.innerHTML = "100$"
-    ar.setAttribute("src", "./catFood/scene.gltf")
+    ar.setAttribute("src", "catFood/scene.gltf")
   }
 }
 window.closeDetails = () => {
